015fb094453b4c76ef0fdf0e87882ed1da97f9f4
Luthpiii <muh.luthfi1105@gmail.com>
2026-04-30 12:14:57 +0800

    Commit 3

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,3 +1,14 @@
 export default function LearnPage() {
-    return <p>ini adalah halaman learn</p>
+    return(
+        <div className="bg-purple-300">
+            <div className="bg-orange-300">
+                <img />
+                <p>Design</p>
+            </div>
+            <div>
+                <h1>Figma</h1>
+                <p>24 hours ago</p>
+            </div>
+        </div>
+    )
 }
