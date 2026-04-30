a00ca51784e2a71d10676dcfb894114e282e6747
Luthpiii <muh.luthfi1105@gmail.com>
2026-04-30 14:56:30 +0800

    Commit 4

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,14 +1,39 @@
 export default function LearnPage() {
     return(
-        <div className="bg-purple-300">
-            <div className="bg-orange-300">
-                <img />
+        <div className="bg-white h-screen flex gap-3 justify-center items-center">
+        <div className="bg-purple-300 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
+            <div className="flex gap-3">
+                <img src="/palette.svg"/>
                 <p>Design</p>
             </div>
             <div>
-                <h1>Figma</h1>
-                <p>24 hours ago</p>
+                <h1 className="text-4xl font-bold">Figma</h1>
+                <p>1 days ago</p>
             </div>
+        </div>
+
+        <div className="bg-orange-300 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
+            <div className="flex gap-3">
+                <img src="/palette.svg"/>
+                <p>Coding</p>
+            </div>
+            <div>
+                <h1 className="text-4xl font-bold">Python</h1>
+                <p>2 days ago</p>
+            </div>
+        </div>
+
+        <div className="bg-pink-300 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
+            <div className="flex gap-3">
+                <img src="/palette.svg"/>
+                <p>Design</p>
+            </div>
+            <div>
+                <h1 className="text-4xl font-bold">Sketch</h1>
+                <p>3 days ago</p>
+            </div>
+        </div>
+
         </div>
     )
 }
