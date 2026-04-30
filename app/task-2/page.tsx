1677a792356427071ec2f0601c2bc06cb57d49c1
Luthpiii <muh.luthfi1105@gmail.com>
2026-04-30 20:58:13 +0800

    Task-2

--- app/task-2/page.tsx
+++ app/task-2/page.tsx
@@ -1,8 +1,6 @@
 export default function LearnPage() {
     return(
-        <div className="bg-white h-screen flex gap-4 justify-center items-center flex-col">
-            <div>
-                <div className="bg-white h-screen flex gap-5 justify-center items-center">
+        <div className="bg-white h-screen flex gap-5 justify-center items-center">
             <div className="bg-cyan-100 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
                 <div className="flex gap-3">
                     <img src="/palette.svg"/>
@@ -54,43 +52,5 @@ export default function LearnPage() {
                 </div>
             </div>
         </div>
-            </div>
-
-                <div className="flex gap-4">
-                    <div className="bg-purple-300 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
-                        <div className="flex gap-3">
-                            <img src="/palette.svg"/>
-                            <p>Design</p>
-                        </div>
-                        <div>
-                            <h1 className="text-4xl font-bold">Figma</h1>
-                            <p>1 days ago</p>
-                        </div>
-                    </div>
-
-                    <div className="bg-orange-300 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
-                        <div className="flex gap-3">
-                            <img src="/code.svg"/>
-                            <p>Coding</p>
-                        </div>
-                        <div>
-                            <h1 className="text-4xl font-bold">Python</h1>
-                            <p>2 days ago</p>
-                        </div>
-                    </div>
-
-                    <div className="bg-pink-300 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
-                        <div className="flex gap-3">
-                            <img src="/palette.svg"/>
-                            <p>Design</p>
-                        </div>
-                        <div>
-                            <h1 className="text-4xl font-bold">Sketch</h1>
-                            <p>3 days ago</p>
-                        </div>
-                    </div>
-                </div>
-            
-        </div>
     )
-}
+};
