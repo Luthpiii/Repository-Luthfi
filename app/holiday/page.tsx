6c8a39cb7921335d637369cbcc23c31e91ae00e4
Luthpiii <muh.luthfi1105@gmail.com>
2026-05-07 11:50:30 +0800

    Quiz

--- app/holiday/page.tsx
+++ app/holiday/page.tsx
@@ -1,15 +1,20 @@
 export default function holidayPage() {
     return (
         <div className="bg-gray-300 h-screen flex justify-center items-center">
-            <div className="bg-white h-115 w-70 rounded-4xl">
+            
+            <div className="bg-white h-115 w-70 rounded-4xl gap-1">
+
                 <div className="p-1">
                     <img src="San Fran.jpg" className=" h-65 rounded-4xl"/>
                 </div>
+
                 <div className="flex flex-col gap-1">
+
                     <div className="p-4">
                         <h1 className="font-bold text-xl">San Francisco</h1>
                         <p className="text-gray-400 text-xs">Premium economy</p>
                     </div>
+
                     <div className="flex p-4 gap-7">
                         <div className="flex gap-2">
                             <img src="/tag.svg" className="h-4 w-4"/>
@@ -20,19 +25,23 @@ export default function holidayPage() {
                             <p className="font-bold text-xs">SFO</p>
                         </div>
                     </div>
+
                     <div className="flex p-3 gap-2">
                         <div className="flex justify-center items-center w-50 h-10 rounded-full bg-black p-4.5">
-                            <button className="text-white">Search flight</button>
+                            <button className="text-white text-xs">Search flight</button>
                         </div>
-                        <div className="flex justify-center items-center h-10 w-10">
-                        <button className="border-white border-2"></button>
+                        <div className="bg-white border border-gray-300  rounded-full flex justify-center items-center h-10 w-10">
+                        <button className=""></button>
                             <div>
-                                <img src="/heart.svg" className="h-8 w-8"/>
+                                <img src="/heart.svg" className=" h-5 w-5"/>
                             </div>
                         </div>
                     </div>
+
                 </div>
+
             </div>
+
         </div>
     )
 }
